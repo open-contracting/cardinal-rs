50d507cc7ccaa11d6baa4023aa251e71427340d5
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-11-16 21:12:43 -0500

    feat: Implement multi-threading

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,14 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::{panic, thread};
 
+use crossbeam::channel::{bounded, Receiver};
 use serde_json::Value;
 
 pub struct Config {
     pub path: String,
+    pub threads: usize,
 }
 
 impl Config {
@@ -18,8 +21,12 @@ impl Config {
             None => return Err("Missing argument 'FILENAME'."),
         };
 
+        // TODO Make configurable.
+        let threads = num_cpus::get();
+
         Ok(Config {
-            path
+            path,
+            threads,
         })
     }
 }
@@ -36,30 +43,58 @@ impl Coverage {
     }
 
     pub fn build(config: Config) -> Result<Coverage, Box<dyn Error>> {
-        // Compiled releases of multiple MiBs have been observed, but most are less than 1 MiB.
-        const CAPACITY: usize = 1024 * 1024;
+        // TODO Understand why this annotation is required.
+        let mut threads: Vec<thread::JoinHandle<Result<Coverage, serde_json::Error>>> = vec![];
+        let (sender, receiver_) = bounded(1024);
 
-        let file = File::open(config.path)?;
-        let mut line = Vec::with_capacity(CAPACITY);
-        let mut reader = BufReader::new(file);
-        let mut coverage = Coverage::new();
+        for _ in 0..config.threads {
+            let receiver: Receiver<String> = receiver_.clone();
 
-        while reader.read_until(b'\n', &mut line).unwrap_or(0) > 0 {
-            let value: Value = serde_json::from_slice(&line)?;
+            threads.push(thread::spawn(|| {
+                let mut coverage = Coverage::new();
 
-            if value.is_object() {
-                coverage.add(value, &mut Vec::with_capacity(16));
-            } else {
-                // TODO return Err() feedback about format
-            }
+                for line in receiver {
+                    let value: Value = serde_json::from_str(&line)?;
+                    if value.is_object() {
+                        coverage.add(value, &mut Vec::with_capacity(16));
+                    } else {
+                        // TODO return Err() feedback about format
+                    }
+                }
 
-            line.clear();
+                Ok(coverage)
+            }));
         }
 
-        Ok(coverage)
+        let file = File::open(config.path)?;
+        let reader = BufReader::new(file);
+
+        for line in reader.lines() {
+            sender.send(line?)?;
+        }
+
+        // Drop the sender, to close the channel.
+        drop(sender);
+
+        let mut total_coverage = Coverage::new();
+
+        for thread in threads {
+            match thread.join() {
+                Ok(result) => {
+                    if let Ok(coverage) = result {
+                        for (k, v) in coverage.counts {
+                            total_coverage.counts.entry(k).and_modify(|count| { *count += v }).or_insert(v);
+                        }
+                    }
+                },
+                Err(e) => panic::resume_unwind(e),
+            }
+        }
+
+        Ok(total_coverage)
     }
 
-    // The longest path has 6 parts (as above or contracts/implementation/transactions/payer/identifier/id).
+    // The longest path has 6 parts (as below or contracts/implementation/transactions/payer/identifier/id).
     // The longest pointer has 10 parts (contracts/0/amendments/0/unstructuredChanges/0/oldValue/classifications/0/id).
     fn add(&mut self, value: Value, path: &mut Vec<String>) {
         match value {
