8766c1e732d9ea0f41bd4173f6f0b3dc0e35252e
James McKinney <26463+jpmckinney@users.noreply.github.com>
2022-11-17 15:21:13 -0500

    test: Add initial tests

---
+++ build.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
+use glob::glob;
+
+// https://stackoverflow.com/a/49056967/244258
+fn main() {
+    let path = Path::new(&env::var("OUT_DIR").unwrap()).join("tests.rs");
+    let mut file = File::create(path).unwrap();
+
+    for entry in glob("tests/fixtures/*.jsonl").expect("Failed to read glob pattern") {
+        let path = entry.unwrap();
+        let input = path.file_name().unwrap().to_str().unwrap();
+        let stem = path.file_stem().unwrap().to_str().unwrap();
+        let output = format!("{stem}.expected");
+        write!(file, r#"
+#[test]
+fn test_{name}() -> Result<(), Box<dyn Error>> {{
+    check("{input}", "{output}")
+}}
+"#, name=stem, input=input, output=output).unwrap()
+    }
+}
---
+++ tests/test.rs
@@ -0,0 +1,28 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+use serde_json;
+
+fn check(input: &str, output: &str) -> Result<(), Box<dyn Error>> {
+    let fixtures = Path::new("tests/fixtures");
+
+    if output.starts_with("invalid_") {
+        // TODO
+    } else {
+        let outpath = fixtures.join(output);
+        let file = File::open(outpath)?;
+        let reader = BufReader::new(file);
+        let expected = serde_json::from_reader(reader)?;
+
+        let inpath = fixtures.join(input);
+        let coverage = libocdscardinal::Coverage::run(inpath, 2)?;
+
+        assert_eq!(coverage.counts, expected);
+    }
+
+    Ok(())
+}
+
+include!(concat!(env!("OUT_DIR"), "/tests.rs"));
