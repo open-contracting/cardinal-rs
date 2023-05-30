02944632789041b823ce9c2f6eaa962155a0d7ce
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-05-30 01:10:52 -0400

    build: Iterate the version number

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "Cardinal"
 copyright = "2002, Open Contracting Partnership"
 author = "Open Contracting Partnership"
 
-version = "0.0.3"
+version = "0.0.4"
 release = version
 
 # -- General configuration ---------------------------------------------------
